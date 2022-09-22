16176d178cc90ec896c0b5641cc373f25b46e094
Fabio Andrijauskas <biozit@gmail.com>
2022-09-22 10:21:04 -0700

    v10

---
+++ var/lib/gwms-frontend/web-base/setup_script 3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2009 Fermi Research Alliance, LLC
+# SPDX-License-Identifier: Apache-2.0
+
+#
+# Project:
+#   glideinWMS
+#
+# File Version: 
+#
+
+glidein_config="$1"
+tmp_fname="${glidein_config}.$$.tmp"
+
+GLIDEIN_WORK_DIR="`grep '^GLIDEIN_WORK_DIR ' "$glidein_config" | cut -d ' ' -f 2-`"
+chmod u+x "$GLIDEIN_WORK_DIR/error_gen.sh"
+
+echo "ERROR_GEN_PATH $GLIDEIN_WORK_DIR/error_gen.sh" >> "$glidein_config"
